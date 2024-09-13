b972f80a69ed61e7daed702352a9e5da3763f415
woorim035 <tropicalwr@naver.com>
2024-09-13 12:29:29 +0900

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,9 +1,9 @@
-def print_hello():
-    print("Hello World!!")
-
+def add(x, y):
+    a = x + y
+    return a
 def print_KIT():
-    print("Kumoh National Institute of Technology")
+    print("KIT")
 
 if __name__ == '__main__':
     print_KIT()
-    print_hello()
+    print(1,2)
